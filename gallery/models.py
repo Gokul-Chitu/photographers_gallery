8511d4e6498af03f974a8988553affcdaa00a1c7
Gokul-Chitu <gokulkrishnan.b.lc@kct.ac.in>
2017-09-23 18:21:58 +0530

    Photographers Gallery, first commit

---
+++ gallery/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=255)
+    photographer_name = models.CharField(max_length=255)
+    photo_place = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    description = models.TextField()
+    camera_used = models.CharField(max_length=255)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
